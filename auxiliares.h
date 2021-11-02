eb26f8dbe6f744d95244409838ae477e65e36218
santiagoromero20 <santiagooromero1234@gmail.com>
2021-11-02 11:18:29 -0300

    primeros auxiliares def

--- auxiliares.h
+++ auxiliares.h
@@ -3,5 +3,33 @@
 
 #include "definiciones.h"
 
+bool esValida(th
+: eph_h, ti: eph_i);
+bool esMatriz(t
+: vector<vector<int>>);
+bool individuoEnTabla(ind
+: individuo, ti:eph_i);
+bool hogarEnTabla(h
+: hogar,th: eph_h);
+bool vacia(t
+: vector<vector<int>>);
+bool cantidadCorrectaDeColumnasI(ti
+: eph_i);
+bool cantidadCorrectaDeColumnasH(th
+: eph_h);
+bool hayIndividuosSinHogares(ti
+:eph_i, th:eph_h);
+bool hayHogaresConCodigo(th
+: eph_h, c:int);
+bool hayHogaresSinIndividuos(ti
+: eph_i, th:eph_h);
+bool hayIndividuoConCodigo(ti
+:eph_i, c:int);
+bool hayRepetidosI(ti
+: eph_i);
+bool mismoCodusuYComponente(i1
+: individuo, i2:individuo);
+bool hayRepetidosH(th
+: eph_h);
 
 #endif //SOLUCION_AUXILIARES_H
