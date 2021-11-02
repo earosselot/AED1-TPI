b8978c0070ad837fd9a0d0b3a082e917d75ed12a
earosselot <earosselot@gmail.com>
2021-11-02 11:34:49 -0300

    auxiliares headers individuo y hogarEnTabla

--- auxiliares.h
+++ auxiliares.h
@@ -3,5 +3,28 @@
 
 #include "definiciones.h"
 
+bool mismoAñoYTrimestre(eph_i ti, eph_h th);
+
+bool menosDe21MiembrosPorHogar(eph_h th, eph_i ti);
+
+bool cantHabitantes(hogar h, eph_i ti);
+
+bool enSuHogar(hogar h, eph_i ti);
+
+bool cantidadValidaDormitorios(eph_h th);
+
+bool valoresEnRangoI(eph_i ti);
+
+bool individuoValido(individuo i);
+
+bool valoresEnRangoH(eph_h th);
+
+bool hogarValido(hogar h);
+
+bool valorRegionValido(int r);
+
+bool esCasa(hogar h);
+
+int ingresos(hogar h, eph_i ti);
 
 #endif //SOLUCION_AUXILIARES_H
