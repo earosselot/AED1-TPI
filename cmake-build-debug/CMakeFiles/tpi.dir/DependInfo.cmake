7ed457ddb8d898048ca654a20e2ca8b2829d1b94
earosselot <earosselot@gmail.com>
2021-11-02 08:36:20 -0300

    primer commit

---
+++ cmake-build-debug/CMakeFiles/tpi.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/earosselot/Documents/repos/AED1-TPI/Funciones_TPI.cpp" "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/CMakeFiles/tpi.dir/Funciones_TPI.o"
+  "/home/earosselot/Documents/repos/AED1-TPI/auxiliares.cpp" "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/CMakeFiles/tpi.dir/auxiliares.o"
+  "/home/earosselot/Documents/repos/AED1-TPI/ejercicios.cpp" "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/CMakeFiles/tpi.dir/ejercicios.o"
+  "/home/earosselot/Documents/repos/AED1-TPI/main.cpp" "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/CMakeFiles/tpi.dir/main.o"
+  "/home/earosselot/Documents/repos/AED1-TPI/test/corregirRegionTEST.cpp" "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/CMakeFiles/tpi.dir/test/corregirRegionTEST.o"
+  "/home/earosselot/Documents/repos/AED1-TPI/test/costoSubsidioMejoraTEST.cpp" "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/CMakeFiles/tpi.dir/test/costoSubsidioMejoraTEST.o"
+  "/home/earosselot/Documents/repos/AED1-TPI/test/creceElTeleworkingEnCiudadesGrandesTEST.cpp" "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/CMakeFiles/tpi.dir/test/creceElTeleworkingEnCiudadesGrandesTEST.o"
+  "/home/earosselot/Documents/repos/AED1-TPI/test/esEncuestaValidaTEST.cpp" "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/CMakeFiles/tpi.dir/test/esEncuestaValidaTEST.o"
+  "/home/earosselot/Documents/repos/AED1-TPI/test/generarJoinTEST.cpp" "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/CMakeFiles/tpi.dir/test/generarJoinTEST.o"
+  "/home/earosselot/Documents/repos/AED1-TPI/test/histHabitacionalTEST.cpp" "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/CMakeFiles/tpi.dir/test/histHabitacionalTEST.o"
+  "/home/earosselot/Documents/repos/AED1-TPI/test/histogramaDeAnillosConcentricosTEST.cpp" "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/CMakeFiles/tpi.dir/test/histogramaDeAnillosConcentricosTEST.o"
+  "/home/earosselot/Documents/repos/AED1-TPI/test/laCasaEstaQuedandoChicaTEST.cpp" "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/CMakeFiles/tpi.dir/test/laCasaEstaQuedandoChicaTEST.o"
+  "/home/earosselot/Documents/repos/AED1-TPI/test/muestraHomogeneaTEST.cpp" "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/CMakeFiles/tpi.dir/test/muestraHomogeneaTEST.o"
+  "/home/earosselot/Documents/repos/AED1-TPI/test/ordenarRegionYCODUSUTEST.cpp" "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/CMakeFiles/tpi.dir/test/ordenarRegionYCODUSUTEST.o"
+  "/home/earosselot/Documents/repos/AED1-TPI/test/quitarIndividuosTEST.cpp" "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/CMakeFiles/tpi.dir/test/quitarIndividuosTEST.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/googletest-master/googletest/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/lib/googletest-master/googlemock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/home/earosselot/Documents/repos/AED1-TPI/cmake-build-debug/lib/googletest-master/googlemock/gtest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/tpi.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "../tpi"
+  "../tpi.pdb"
+  "CMakeFiles/tpi.dir/Funciones_TPI.o"
+  "CMakeFiles/tpi.dir/auxiliares.o"
+  "CMakeFiles/tpi.dir/ejercicios.o"
+  "CMakeFiles/tpi.dir/main.o"
+  "CMakeFiles/tpi.dir/test/corregirRegionTEST.o"
+  "CMakeFiles/tpi.dir/test/costoSubsidioMejoraTEST.o"
+  "CMakeFiles/tpi.dir/test/creceElTeleworkingEnCiudadesGrandesTEST.o"
+  "CMakeFiles/tpi.dir/test/esEncuestaValidaTEST.o"
+  "CMakeFiles/tpi.dir/test/generarJoinTEST.o"
+  "CMakeFiles/tpi.dir/test/histHabitacionalTEST.o"
+  "CMakeFiles/tpi.dir/test/histogramaDeAnillosConcentricosTEST.o"
+  "CMakeFiles/tpi.dir/test/laCasaEstaQuedandoChicaTEST.o"
+  "CMakeFiles/tpi.dir/test/muestraHomogeneaTEST.o"
+  "CMakeFiles/tpi.dir/test/ordenarRegionYCODUSUTEST.o"
+  "CMakeFiles/tpi.dir/test/quitarIndividuosTEST.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tpi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
