6946c7b7b6f8c9fe47d921905a6ae7ec96e78b70
julianmagi <magijulian0@gmail.com>
2021-11-02 11:25:50 -0300

    esMatriz

--- auxiliares.cpp
+++ auxiliares.cpp
@@ -3,4 +3,19 @@
 #include "gtest/gtest.h"
 #include "ejercicios.h"
 
+bool esMatriz(vector<vector<int> > v) {
+    int i = 0;
+    int j = 0;
 
+    while (i < v.size() - 1) {
+        if (v[i].size() != v[i + 1].size()) {
+            j = j + 1;
+            i = i + 1;
+        } else
+            i = i + 1;
+    }
+    if (j == 0) {
+        return true;
+    } else
+        return false;
+}
